3d1200af7db2cb3a239f491f1e5bd0db6e69d7ad
Simon Willison <swillison@gmail.com>
2020-07-13 11:30:43 -0700

    Utility functions for hashing passwords, refs #1

---
+++ datasette_auth_passwords/utils.py
@@ -0,0 +1,29 @@
+import base64
+import hashlib
+import secrets
+
+ALGORITHM = "pbkdf2_sha256"
+
+
+def pbkdf2(password_bytes, salt_bytes, iterations):
+    return
+
+
+def hash_password(password, salt=None, iterations=260000):
+    if salt is None:
+        salt = secrets.token_hex(16)
+    assert salt and isinstance(salt, str) and "$" not in salt
+    assert password and isinstance(password, str)
+    pw_hash = hashlib.pbkdf2_hmac(
+        "sha256", password.encode("utf-8"), salt.encode("utf-8"), iterations
+    )
+    b64_hash = base64.b64encode(pw_hash).decode("ascii").strip()
+    return "{}${}${}${}".format(ALGORITHM, iterations, salt, b64_hash)
+
+
+def verify_password(password, password_hash):
+    algorithm, iterations, salt, b64_hash = password_hash.split("$", 3)
+    iterations = int(iterations)
+    assert algorithm == ALGORITHM
+    compare_hash = hash_password(password, salt, iterations)
+    return secrets.compare_digest(password_hash, compare_hash)
--- tests/test_auth_passwords.py
+++ tests/test_auth_passwords.py
@@ -1,4 +1,5 @@
 from datasette.app import Datasette
+from datasette_auth_passwords import utils
 import pytest
 import httpx
 
@@ -11,3 +12,18 @@ async def test_plugin_is_installed():
         assert 200 == response.status_code
         installed_plugins = {p["name"] for p in response.json()}
         assert "datasette-auth-passwords" in installed_plugins
+
+
+def test_utils_hash_password():
+    hashed_password = utils.hash_password("hello")
+    assert hashed_password.count("$") == 3
+    assert hashed_password.startswith("pbkdf2_sha256$")
+    # Running same agains should return a different password
+    hashed_password2 = utils.hash_password("hello")
+    assert hashed_password2 != hashed_password
+
+
+def test_verify_password():
+    hashed_password = utils.hash_password("hello")
+    assert utils.verify_password("hello", hashed_password)
+    assert not utils.verify_password("hello2", hashed_password)
