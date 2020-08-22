c9bc7ad80905e20535b6659d9a3e94b4b301f118
Simon Willison <swillison@gmail.com>
2020-08-21 21:07:30 -0700

    Depends on datasette>=0.44
    Refs #12

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     version=VERSION,
     packages=["datasette_auth_passwords"],
     entry_points={"datasette": ["auth_passwords = datasette_auth_passwords"]},
-    install_requires=["datasette"],
+    install_requires=["datasette>=0.44"],
     extras_require={"test": ["pytest", "pytest-asyncio", "httpx"]},
     tests_require=["datasette-auth-passwords[test]"],
     package_data={"datasette_auth_passwords": ["templates/*.html",]},
